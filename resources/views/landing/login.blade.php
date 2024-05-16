ded2ad8dc5df895b851d9255a486f40e19f53420
Rafi Enha <120245548+RafiEnha@users.noreply.github.com>
2024-05-16 16:54:04 +0700

    Update login.blade.php

--- resources/views/landing/login.blade.php
+++ resources/views/landing/login.blade.php
@@ -128,7 +128,7 @@
   </div><!-- End Modal -->
 
   <section id="login" class="mt-5 pb-0">
-    <div class="container">
+    <div class="container w-75 px-5">
 
       <p><a class="text-black" href="index.html">Beranda</a> / <a href="">Login</a></p>
 
