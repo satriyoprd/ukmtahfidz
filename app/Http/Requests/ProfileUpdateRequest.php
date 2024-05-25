4e954a89ad5de3fca5cf2827e086f50f74204258
MaritsIkmalY <maritsikmaly@gmail.com>
2024-05-25 22:02:15 +0700

    refactor: Update ProfileUpdateRequest to include phone field validation

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,9 +18,9 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'jumlah_hafalan' => ['required'],
             'jenis_kelamin' => ['required'],
             'tanggal_lahir' => ['required', 'date'],
+            'phone' => ['required', 'string', 'max:255'],
         ];
     }
 }
